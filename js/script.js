e4995e2327e5cb2bb659f5ff0d55e4045392c059
sashapoliti <sas.politi@gmail.com>
2024-04-02 16:08:16 +0200

    add ul with dynamic li and Toggle & Remove methods

--- js/script.js
+++ js/script.js
@@ -8,7 +8,20 @@ createApp({
       toDo,
     };
   },
-  methods: {},
+  methods: {
+    toggleDone(id) {
+      console.log("Toggle done method called");
+      const index = this.toDo.findIndex((el) =>  el.id === id); //trovo index corrispondente a id
+      
+      if (index !== -1) { this.toDo[index].done = !this.toDo[index].done; }; //controllo se l'elemento è presente e cambio
+    },
+    removeItem(id) {
+      console.log("Remove method called");
+      const index = this.toDo.findIndex((el) =>  el.id === id); //trovo index corrispondente a id
+
+      if (index !== -1) { this.toDo.splice(index, 1); }; //controllo se l'elemento è presente ed elimino
+    }
+  },
   computed: {},
   mounted() {},
 }).mount("#app");
