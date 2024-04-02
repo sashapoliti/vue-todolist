aa8641e0afad0fa3b383ef3a06f75927d6075233
sashapoliti <sas.politi@gmail.com>
2024-04-02 16:17:43 +0200

    add input and new method add Item for add new to do to the list

--- js/script.js
+++ js/script.js
@@ -6,20 +6,37 @@ createApp({
   data() {
     return {
       toDo,
+      itemText : ''
     };
   },
   methods: {
     toggleDone(id) {
-      console.log("Toggle done method called");
-      const index = this.toDo.findIndex((el) =>  el.id === id); //trovo index corrispondente a id
+      /* console.log("Toggle done method called"); */
+      const index = this.toDo.findIndex((el) =>  el.id === id); //find index
       
-      if (index !== -1) { this.toDo[index].done = !this.toDo[index].done; }; //controllo se l'elemento è presente e cambio
+      if (index !== -1) { this.toDo[index].done = !this.toDo[index].done; }; //control
     },
     removeItem(id) {
-      console.log("Remove method called");
-      const index = this.toDo.findIndex((el) =>  el.id === id); //trovo index corrispondente a id
+      /* console.log("Remove method called"); */
+      const index = this.toDo.findIndex((el) =>  el.id === id); //find index
 
-      if (index !== -1) { this.toDo.splice(index, 1); }; //controllo se l'elemento è presente ed elimino
+      if (index !== -1) { this.toDo.splice(index, 1); }; //control
+    },
+    addItem() {
+      const newItem = {
+        id : null,
+        text : this.itemText,
+        done : false
+      };
+      let nextId = 0;
+      this.toDo.forEach(element => {
+        if(nextId < element.id) {
+          nextId = element.id;
+        }
+      });
+      newItem.id = nextId + 1;
+      this.toDo.push(newItem);
+      this.itemText = '';
     }
   },
   computed: {},
